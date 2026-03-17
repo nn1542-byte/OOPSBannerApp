287c306d9506bb7877b39f846a453dce95e7f2d4
nn1542-byte <nn1542@srmist.edu.in>
2026-03-17 09:10:18 +0530

    UC6

---
+++ BannerAppUC6.java/UC6.java
@@ -0,0 +1,32 @@
+public class UC6 {
+    public static void main(String[] args) {
+        // UC6: Use static helper methods to populate the array
+        String[] banner = new String[7];
+        
+        for (int i = 0; i < 7; i++) {
+            // Invoking methods to build each line
+            banner[i] = getO(i) + " " + getO(i) + " " + getP(i) + " " + getS(i);
+        }
+
+        // Loop-based rendering system
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    // Static helper methods for character patterns
+    public static String getO(int row) {
+        String[] pattern = {" * ", "* ", " ", " ", " ", " ", " ** "};
+        return pattern[row];
+    }
+
+    public static String getP(int row) {
+        String[] pattern = {"** ", "* ", " ", "* ", "* ", "* ", "* "};
+        return pattern[row];
+    }
+
+    public static String getS(int row) {
+        String[] pattern = {" ** ", "* ", "* ", " * ", "    * ", "    * ", "** "};
+        return pattern[row];
+    }
+}
